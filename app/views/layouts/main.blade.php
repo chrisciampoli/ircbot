70afd5a89d0d09fba26a8df961cc9b3e7ce224ed
chrisciampoli <chrisciampoli@gmail.com>
2013-12-19 12:36:13 -0800

    changes

--- app/views/layouts/main.blade.php
+++ app/views/layouts/main.blade.php
@@ -19,14 +19,12 @@
               <span class="icon-bar"></span>
               <span class="icon-bar"></span>
             </button>
-            <a class="navbar-brand" href="#">Project name</a>
+            <a class="navbar-brand" href="#">Wildrunners IRCBOT</a>
           </div>
           <div class="collapse navbar-collapse">
             <ul class="nav navbar-nav">
-              <li class="active"><a href="#">Home</a></li>
-              <li>{{ HTML::link('users/register', 'Register') }}</li>   
+              <li class="active">{{ HTML::link('users/register', 'Register') }}</li>   
               <li>{{ HTML::link('users/login', 'Login') }}</li>
-              <li>TESTTEST</li>
             </ul>
           </div><!--/.nav-collapse -->
         </div>
