65c5215fd4712954b8c545739e9523b2d49a9176
chrisciampoli <chrisciampoli@gmail.com>
2013-12-19 12:32:18 -0800

    changes

--- app/views/layouts/main.blade.php
+++ app/views/layouts/main.blade.php
@@ -9,6 +9,31 @@
   </head>
  
   <body>
+      <!-- Fixed navbar -->
+      <div class="navbar navbar-default navbar-fixed-top" role="navigation">
+        <div class="container">
+          <div class="navbar-header">
+            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+              <span class="sr-only">Toggle navigation</span>
+              <span class="icon-bar"></span>
+              <span class="icon-bar"></span>
+              <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="#">Project name</a>
+          </div>
+          <div class="collapse navbar-collapse">
+            <ul class="nav navbar-nav">
+              <li class="active"><a href="#">Home</a></li>
+              <li>{{ HTML::link('users/register', 'Register') }}</li>   
+              <li>{{ HTML::link('users/login', 'Login') }}</li>   
+            </ul>
+          </div><!--/.nav-collapse -->
+        </div>
+      </div>
+      
+      
+      
+      
       <div class="navbar navbar-default navbar-fixed-top" role="navigation">
       <div class="navbar-inner">
          <div class="container">
