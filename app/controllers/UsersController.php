0f1d17b00b7fdff3ca16181c4528bf5a9ac1f3e4
chrisciampoli <chrisciampoli@gmail.com>
2013-12-19 22:52:21 -0800

    changes

--- app/controllers/UsersController.php
+++ app/controllers/UsersController.php
@@ -6,10 +6,32 @@
  * @author chris
  */
 class UsersController extends BaseController {
+    
     protected $layout = "layouts.main";
     
     public function getRegister() {
         $this->layout->content = View::make('users.register');
+    }
+    
+    
+    /*
+     * Register a new user
+     */
+    public function postCreate() {
+        $validator = Validator::make(Input::all(), User::$rules);
+        
+        if($validator->passes()) {
+            $user = new User;
+            $user->firstname = Input::get('firstname');
+            $user->lastname = Input::get('lastname');
+            $user->email = Input::get('email');
+            $user->password = Hash::make(Input::get('password'));
+            $user->save(); // LEFT OFF HERE
+            
+        } else {
+            // failed show em errors
+        }
+        
     }
     
 }
