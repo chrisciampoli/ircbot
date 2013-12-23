c0bf7fb9e83d8e516007b17023c40ab507f3f378
chrisciampoli <chrisciampoli@gmail.com>
2013-12-22 21:09:24 -0800

    changes

---
+++ app/controllers/BotController.php
@@ -0,0 +1,17 @@
+<?php
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of BotController
+ *
+ * @author chris
+ */
+class BotController extends BaseController {
+    //put your code here
+}
+
+?>
